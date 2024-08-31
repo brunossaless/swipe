f9fa3f67404ea13f70d2b91e9e331d63290c793f
Bruno Sales <bruno.particular25@hotmail.com>
2024-08-31 14:03:51 -0300

    fix: remove scroll view

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { Deck } from "@/components/Deck";
 import { DATA } from "@/constants/Deck";
-import { SafeAreaView, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native";
 
 export default function Index() {
   return (
@@ -9,9 +9,7 @@ export default function Index() {
         flex: 1,
       }}
     >
-      <ScrollView>
-        <Deck data={DATA} />
-      </ScrollView>
+      <Deck data={DATA} />
     </SafeAreaView>
   );
 }
